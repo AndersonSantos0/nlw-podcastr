0ee780b6ef5d8970e5e3747a2ec710c97a613c63
Anderson Santos <Anderson_Santos__@outlook.com>
2021-04-23 22:17:57 -0300

    baseUrl bugfix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: process.env.API_URL || 'http://localhost:3333/'
+    baseURL: 'https://my-json-server.typicode.com/andersonSantos0/nlw-podcastr'
 })
