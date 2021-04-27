d99fe2f3bba4eb130ba7868c00e230ba09a5f3b0
Anderson Santos <Anderson_Santos__@outlook.com>
2021-04-27 16:53:02 -0300

    logo themed update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,15 +2,17 @@ import React, { useContext } from 'react'
 import format from 'date-fns/format'
 import ptBR from 'date-fns/locale/pt-BR'
 import styles from './styles.module.scss'
-import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { ThemeContext } from 'styled-components'
 import DarkModeSwitcher from '../DarkModeButton'
+import Logo from '../Logo'
 
 interface HeaderProps {
   toggleTheme: () => void
 }
 
-export const Header: React.FC<HeaderProps> = ({toggleTheme}) => {
+export const Header: React.FC<HeaderProps> = ({ toggleTheme }) => {
+  const router = useRouter()
 
   const { title } = useContext(ThemeContext)
 
@@ -20,9 +22,7 @@ export const Header: React.FC<HeaderProps> = ({toggleTheme}) => {
 
   return (
     <header className={styles.headerContainer}>
-      <Link href={'/'}>
-        <img src="/logo.svg" alt="Podcastr" />
-      </Link>
+      <Logo onCLick={() => router.push('/')} />
       <p>O melhor para você ouvir sempre</p>
       <div>
         <span>{currentDate}</span>
